ae781e0fc64b2f727a97af99ea8aec10c09e2251
Robbie <robbiealternate@gmail.com>
2020-11-09 05:11:45 +0000

    Add example search

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,6 @@
-#include <ctime>
-#include <string>
-
 #include "make_move.h"
-#include "move_gen.h"
 #include "move_tables.h"
-
-constexpr uint8_t MAX_DEPTH = 8;
-
-Move ALL_MOVES[MAX_DEPTH][MAX_MOVES];
-
-uint64_t perft(Board *board, int depth, int ply = 0) {
-    if (depth == 0 || board->game_over())
-        return 1;
-
-    uint64_t nodes = 0;
-
-    Move *moves = ALL_MOVES[ply];
-    uint8_t size = gen_moves(board, moves);
-    for (int i = 0; i < size; ++i) {
-        make_move(board, moves[i]);
-        nodes += perft(board, depth - 1, ply + 1);
-        undo_move(board, moves[i]);
-    }
-
-    return nodes;
-}
+#include "search.h"
 
 int main() {
     init_move_tables();
@@ -37,14 +13,7 @@ int main() {
     board.cards[1][1] = ELEPHANT;
     board.side_card = CRAB;
 
-    for (int depth = 0; depth <= MAX_DEPTH; ++depth) {
-        clock_t start = clock();
-        uint64_t result = perft(&board, depth);
-        double duration = (clock() - start) / static_cast<double>(CLOCKS_PER_SEC);
-        double speed = static_cast<double>(result) / duration / 1000000;
-        printf("Depth %i: %10llu nodes (%.5ss, %5.5s Mnps)\n", depth, result,
-               std::to_string(duration).c_str(), std::to_string(speed).c_str());
-    }
+    search(&board);
 
     return 0;
 }
