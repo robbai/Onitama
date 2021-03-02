bcb5116acbd8f147767df549fa060637e9ca45d8
Robbie Heath <robbiealternate@gmail.com>
2021-03-02 19:47:28 +0000

    Keep hands in ascending order

--- src/move_gen.cpp
+++ src/move_gen.cpp
@@ -16,6 +16,9 @@ uint8_t gen_moves(Board *board, Move *moves, Bitboard targets) {
 
         // Iterate through cards.
         for (int card_index = 0; card_index < CARDS_EACH_NUM; ++card_index) {
+            bool lower_swap = (card_index == (board->side_card <
+                                              board->cards[board->turn][1 - card_index]));
+
             // Iterate through move squares and create moves.
             Bitboard squares =
                     MOVE_TABLES[(board->cards[board->turn][card_index] * SQUARE_NUM +
@@ -23,7 +26,8 @@ uint8_t gen_moves(Board *board, Move *moves, Bitboard targets) {
                                 board->turn] &
                     targets;
             Move move = MoveBits::half_create_move(
-                    from, card_index, mask_1 & board->pieces[board->turn][MASTER]);
+                    from, card_index, mask_1 & board->pieces[board->turn][MASTER],
+                    lower_swap);
             while (squares) {
                 uint8_t to = __builtin_ctz(squares);
                 Bitboard mask_2 = (1u << to);
