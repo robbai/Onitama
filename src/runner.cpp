80413fc4cc4273aff0c351a68679d0cbff019c2f
Robbie Heath <robbiealternate@gmail.com>
2021-04-05 15:32:27 +0100

    Add moves runner command

--- src/runner.cpp
+++ src/runner.cpp
@@ -62,7 +62,7 @@ int use_runner() {
             while (stream >> given_move) {
                 uint8_t size = gen_moves(&board, moves);
                 for (uint8_t i = 0; i < size; ++i) {
-                    Move move = moves[i];
+                    const Move move = moves[i];
                     if (move_string(&board, move) == given_move) {
                         make_move(&board, move);
                         break;
@@ -71,6 +71,13 @@ int use_runner() {
                                   << std::endl;
                     }
                 }
+            }
+        } else if (command == "moves") {
+            Move moves[MAX_MOVES];
+            uint8_t size = gen_moves(&board, moves);
+            for (uint8_t i = 0; i < size; ++i) {
+                const Move move = moves[i];
+                std::cout << int(i + 1) << ": " << move_string(&board, move) << std::endl;
             }
         } else if (command == "print") {
             std::cout << pretty_board(&board) << std::endl;
