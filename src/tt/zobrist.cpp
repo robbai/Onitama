75331931bc629c92e3a2556fc0728efbc78bde63
Robbie Heath <robbiealternate@gmail.com>
2022-01-04 15:05:48 +0000

    Use PRNG in Zobrist

--- src/tt/zobrist.cpp
+++ src/tt/zobrist.cpp
@@ -1,27 +1,44 @@
 #include "zobrist.h"
 
-#include <random>
-
 namespace Zobrist {
     Hash PIECES[PLAYERS_NUM][PIECE_TYPES_NUM][SQUARE_NUM];
     Hash CARDS[CARDS_EACH_NUM][PLAYERS_NUM + 1];
     Hash TURN;
 }  // namespace Zobrist
 
+/*
+ * https://stackoverflow.com/a/39675616
+ */
+class XRS_64 {
+ public:
+    XRS_64() {
+        seed = 6394358446697381921;
+    }
+    uint64_t generate(void) {
+        seed ^= seed >> 12;
+        seed ^= seed << 25;
+        seed ^= seed >> 27;
+        return seed * UINT64_C(2685821657736338717);
+    }
+
+ private:
+    uint64_t seed;
+};
+
 void init_zobrist() {
-    std::mt19937_64 gen(std::random_device{}());
+    XRS_64 rng = XRS_64();
     for (int i = 0; i < PLAYERS_NUM; ++i) {
         for (int j = 0; j < PIECE_TYPES_NUM; ++j) {
             for (int k = 0; k < SQUARE_NUM; ++k)
-                Zobrist::PIECES[i][j][k] = gen();
+                Zobrist::PIECES[i][j][k] = rng.generate();
         }
     }
     for (int i = 0; i < CARD_NUM; ++i) {
         for (int j = 0; j < (PLAYERS_NUM + 1); ++j) {
-            Zobrist::CARDS[i][j] = gen();
+            Zobrist::CARDS[i][j] = rng.generate();
         }
     }
-    Zobrist::TURN = gen();
+    Zobrist::TURN = rng.generate();
 }
 
 void set_hash(Board *board) {
